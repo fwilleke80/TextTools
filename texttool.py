f8bc4141955cf749c098f33815772de19749a522
Frank Willeke <fwilleke80@users.noreply.github.com>
2019-03-21 23:44:07 +0100

    Initial checkin

---
+++ texttool.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import optparse
+from textlib import analyze
+
+
+def main():
+    # Title
+    print('TextTools 0.2')
+    print('2019 by Frank Willeke')
+    print(' ')
+
+    # Command Line Options
+    parser = optparse.OptionParser('usage: %prog --option1 arg1 arg2 --option2 arg')
+    parser.add_option('--analyze', type='str', dest='analyze', nargs=1, default=None, metavar='PATH', help='Analyze a text and create extensive metadata.')
+    (options, args) = parser.parse_args()
+
+    # Text analysis
+    if options.analyze:
+        analyze.analyze(options.analyze)
+        return
+
+    parser.print_help()
+
+
+# Kick off the shit...
+if __name__=='__main__':
+    try:
+        print('')
+        main()
+        print('')
+    except KeyboardInterrupt:
+        print('Cancelled')
