c2fb111aeaf0b65fcc5a3812b4bd95fc44c017af
Frank Willeke <github@frankwilleke.de>
2019-04-18 23:55:46 +0200

    Cleanup; added timer

--- texttool.py
+++ texttool.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import time
 import optparse
 from textlib import analyze,csm,fun
 
@@ -16,25 +17,40 @@ def main():
 
     # Command Line Options
     parser = optparse.OptionParser('usage: %prog --option1 arg1 arg2 --option2 arg')
-    parser.add_option('-a', '--analyze', type='str', dest='analyze', nargs=1, default=None, metavar='PATH', help='Analyze a text and create extensive metadata')
-    parser.add_option('-c', '--csm', type='str', dest='commonSense', nargs=1, default=None, metavar='MODE PATH', help='Learns or evaluates Common Sense Matrices. Use "--csm help" for more information.')
-    parser.add_option('-f', '--fun', type='str', dest='fun', nargs=1, default=None, metavar='FUN', help='Fun with words')
-    parser.add_option('-l', '--language', type='str', dest='language', nargs=1, default='de_DE', metavar='ISOLANGUAGE', help='Define the language of the texts to analyze ("de_DE", "en_US", et cetera). If unspecified, "' + LANG_DEFAULT + '" is used.')
+    parser.add_option('-a', '--analyze', type='str', dest='analyze', nargs=1, default=None,
+                      metavar='PATH', help='Analyze a text and create extensive metadata')
+    parser.add_option('-l', '--language', type='str', dest='language', nargs=1, default='de_DE', metavar='ISOLANGUAGE',
+                      help='Define the language of the texts to analyze ("de_DE", "en_US", et cetera). If unspecified, "' + LANG_DEFAULT + '" is used.')
+    parser.add_option('-c', '--csm', type='str', dest='commonSense', nargs=1, default=None, metavar='MODE PATH',
+                      help='Learns or evaluates Common Sense Matrices. Use "--csm help" for more information.')
+    parser.add_option('-s', '--shuffle', type='str', dest='fun',
+                      nargs=1, default=None, metavar='FUN', help='Fun with words')
+    parser.add_option('-f', '--force', action='store_true', dest='force', default=False, help='Force update of cached data')
     (options, args) = parser.parse_args()
 
-    # Text analysis
-    if options.analyze:
-        analyze.analyze(options.analyze, fileExtension='.txt', lang=options.language)
-        return
+    # Memorize start time
+    timeStarted = time.time()
 
+    # Text analysis
+    doneSomething = False
+    if options.analyze:
+        analyze.analyze(options.analyze, fileExtension='.txt', lang=options.language, forceAnalyze=options.force)
+        doneSomething = True
+
+    # Common Sense Matrix
     if options.commonSense:
         csm.start(options.commonSense, args)
-        return 
+        doneSomething = True
+        
+    # Word Shuffle Fun
     if options.fun:
         fun.have_fun(options.fun, lang=options.language)
-        return
+        doneSomething = True
 
-    parser.print_help()
+    if not doneSomething:
+        parser.print_help()
+    else:
+        print('\nRuntime: ' + "{:.3f}".format(time.time() - timeStarted) + ' seconds.')
 
 
 # Kick off the shit...
